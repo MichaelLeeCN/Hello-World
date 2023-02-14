1ac8943d85b320b95e95e54b887311309ccb3c4c
Michael Lee <michaelleecn@outlook.com>
2023-02-14 21:33:30 +0800

    20230214 first submit

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: Michael Lee
+ * @Date: 2023-02-11 12:15:36
+ * @LastEditors: Michael Lee
+ * @LastEditTime: 2023-02-11 12:15:58
+ * @Description: 
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello,world"<<endl;
+    return 0;
+}
